7410c073ba74c2fe7e1a9c0f725774cb8ab48fc8
Adam Cohn <adamcohn88@gmail.com>
2019-01-02 10:49:43 -0500

    resume from cms

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -6,31 +6,17 @@ import ExperienceSubHeader from './ExperienceSubheader'
 import ExperienceListItem from './ExperienceListItem'
 import ExperienceList from './ExperienceList'
 
-const Experience = () => (
+const Experience = ({ title, dates, tasks }) => (
   <Experience.Container>
     <Experience.Content>
-      <Experience.Header>Political Director</Experience.Header>
-      <Experience.Subheader>March 2016 - April 2018</Experience.Subheader>
+      <Experience.Header>{title}</Experience.Header>
+      <Experience.Subheader>{dates}</Experience.Subheader>
       <ExperienceList>
-        <Experience.ListItem>
-          Managed team of analysts and fellows producing Atlas Project Roadmap
-          Reports, post-election analysis, weekly newsletters, and developing
-          the Online Toolkit
-        </Experience.ListItem>
-
-        <Experience.ListItem>
-          Planned and executed ad hoc research projects from Atlas clients
-          including labor unions, political action committees, and consultants
-        </Experience.ListItem>
-
-        <Experience.ListItem>
-          Analyzed voter registration, polling, economic, media spending, and
-          demographic data for reports and blog posts
-        </Experience.ListItem>
-
-        <Experience.ListItem>
-          Edited, revised, and provided feedback on colleagues’ written work
-        </Experience.ListItem>
+        {tasks.map((task, index) => (
+          <Experience.ListItem key={`${title}${task}${index}`}>
+            {task.role.text}
+          </Experience.ListItem>
+        ))}
       </ExperienceList>
     </Experience.Content>
   </Experience.Container>
--- src/pages/resume.js
+++ src/pages/resume.js
@@ -12,49 +12,87 @@ import ExperienceCompanySubheader from '../components/Experience/ExperienceCompa
 import Skills from '../components/Skills/Skills'
 import Education from '../components/Education/Education'
 
-const ResumePage = () => (
-  <Layout>
-    <SEO title="Resume" keywords={['gatsby', 'application', 'react']} />
-    <Container>
-      <ExperienceSectionHeader>Experience</ExperienceSectionHeader>
-      <ExperienceCompanyHeader>The Atlas Project</ExperienceCompanyHeader>
-      <ExperienceCompanySubheader>Washington, D.C.</ExperienceCompanySubheader>
-      <ExperiencesWrapper>
-        <Experience />
-        <Experience />
-        <Experience />
-        <Experience />
-      </ExperiencesWrapper>
-      <ExperienceCompanyHeader>
-        Maryland Secretary of State
-      </ExperienceCompanyHeader>
-      <ExperienceCompanySubheader>
-        Annapolis, Maryland
-      </ExperienceCompanySubheader>
-      <ExperiencesWrapper>
-        <Experience />
-      </ExperiencesWrapper>
-      <ExperienceCompanyHeader>
-        Friends of Martin O'Malley
-      </ExperienceCompanyHeader>
-      <ExperienceCompanySubheader>
-        Baltimore, Maryland
-      </ExperienceCompanySubheader>
-      <ExperiencesWrapper>
-        <Experience />
-      </ExperiencesWrapper>
-      <ExperienceSectionHeader>Skills</ExperienceSectionHeader>
-      <ExperiencesWrapper>
-        <Skills />
-        <Skills />
-      </ExperiencesWrapper>
-      <ExperienceSectionHeader>Education</ExperienceSectionHeader>
-      <ExperiencesWrapper>
-        <Education />
-        <Education />
-      </ExperiencesWrapper>
-    </Container>
-  </Layout>
-)
+const ResumePage = ({ data: { prismicResume } }) => {
+  const resume = prismicResume.data.body
+
+  return (
+    <Layout>
+      <SEO title="Resume" keywords={['gatsby', 'application', 'react']} />
+      <Container>
+        <ExperienceSectionHeader>Experience</ExperienceSectionHeader>
+        {resume.map((job, index) => (
+          <div key={`${job}${index}`}>
+            <ExperienceCompanyHeader>
+              {job.primary.employer.text}
+            </ExperienceCompanyHeader>
+            <ExperienceCompanySubheader>
+              {job.primary.location.text}
+            </ExperienceCompanySubheader>
+            <ExperiencesWrapper>
+              {job.items.map((role, index) => (
+                <Experience
+                  key={`${role}${index}`}
+                  title={role.title.text}
+                  dates={role.dates.text}
+                  tasks={role.tasks.document[0].data.body[0].items}
+                />
+              ))}
+            </ExperiencesWrapper>
+          </div>
+        ))}
+        <ExperienceSectionHeader>Skills</ExperienceSectionHeader>
+        <ExperiencesWrapper>
+          <Skills />
+          <Skills />
+        </ExperiencesWrapper>
+        <ExperienceSectionHeader>Education</ExperienceSectionHeader>
+        <ExperiencesWrapper>
+          <Education />
+          <Education />
+        </ExperiencesWrapper>
+      </Container>
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  {
+    prismicResume {
+      data {
+        body {
+          primary {
+            employer {
+              text
+            }
+            location {
+              text
+            }
+          }
+          items {
+            title {
+              text
+            }
+            dates {
+              text
+            }
+            tasks {
+              document {
+                data {
+                  body {
+                    items {
+                      role {
+                        text
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default ResumePage
