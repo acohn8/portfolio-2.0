60915dcf0c4d0980afea72b64d564d3de3e177ee
Adam Cohn <adamcohn88@gmail.com>
2019-03-14 19:32:13 -0400

    smaller social icons in mobile

--- src/components/SocialIconContainer/SocialIconImage.js
+++ src/components/SocialIconContainer/SocialIconImage.js
@@ -1,11 +1,18 @@
 import styled from 'styled-components'
 
 const SocialIconImage = styled.img`
-  width: 50px;
-  height: 50px;
-  padding: 10px 5px;
   margin: 0;
   background-color: ${props => (props.linkedin ? '#0077BA' : '#333334')};
+  @media only screen and (min-width: 767px) {
+    width: 50px;
+    height: 50px;
+    padding: 10px 5px;
+  }
+  @media only screen and (max-width: 767px) {
+    width: 30px;
+    height: 30px;
+    padding: 5px;
+  }
 `
 
 export default SocialIconImage
